6808875125060c4af5d72d9e35731dbc395ba238
nandoi0 <97204056+nandoi0@users.noreply.github.com>
2022-07-30 23:25:21 -0400

    accounts app

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,8 @@ import customer
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('customer.urls')),
+    path('accounts/', include('django.contrib.auth.urls')), 
+    path('accounts/', include('accounts.urls')),
     
 ]
 
